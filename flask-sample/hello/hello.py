082b25f11cb582fe3b4b49918b5e2b6de60f711a
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-04 15:50:58 -0800

    Added Flaks routing with argument

--- flask-sample/hello/hello.py
+++ flask-sample/hello/hello.py
@@ -5,9 +5,24 @@ app = Flask(__name__)
 def index():
 	return 'Index Page'
 
+# simple routing example
 @app.route('/hello')
 def hello():
 	return 'Hello!'
+
+# routing with argument
+@app.route('/user/<username>')
+def show_user_profile(username):
+	# show the user profile for that user
+	return 'User %s' % username
+
+# routing with type defined argument
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	return 'Post %s' % post_id 
+
+
+
 
 if __name__ == '__main__':
 	app.run()
