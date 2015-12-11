761e8cee6d0dd96374c402f60dc014191b4843fc
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-11 12:40:49 -0800

    Added flask app initialisation and database first definition

---
+++ payblog/blog/app/app.py
@@ -0,0 +1,71 @@
+# app.py
+
+import datetime
+import functools
+import re
+import os
+import urllib
+
+from flask import (Flask, abort, flash, Markup, redirect, render_template,
+                   request, Response, session, url_for)
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+from peewee import *
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.sqlite_ext import *
+
+ADMIN_PASSWORD = 'secret'
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
+DEBUG = False
+SECRET_KEY = 'shhh, secret!'  # Used by Flask to encrypt session cookie.
+SITE_WIDTH = 800
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+flask_db = FlaskDB(app)
+database = flask_db.database
+
+oembed_providers = bootstrap_basic(OEmbedCache())
+
+
+# Data base modeling
+class Entry(flask_db.Model):
+    title = CharField()
+    slug = CharField(unique=True)
+    content = TextField()
+    published = BooleanField(index=True)
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = re.sub('[^\w]+', '-', self.title.lower())
+        ret = super(Entry, self).save(*args, **kwargs)
+
+        # Store search content.
+        self.update_search_index()
+        return ret
+
+    def update_search_index(self):
+        try:
+            fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
+        except FTSEntry.DoesNotExist:
+            fts_entry = FTSEntry(entry_id=self.id)
+            force_insert = True
+        else:
+            force_insert = False
+        fts_entry.content = '\n'.join((self.title, self.content))
+        fts_entry.save(force_insert=force_insert)
+
+
+class FTSEntry(FTSModel):
+    entry_id = IntegerField()
+    content = TextField()
+
+    class Meta:
+        database = database
