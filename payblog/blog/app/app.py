7cb0be196e95e6e3c3fcd44e4e65e2bd0bbfbfd5
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-11 19:42:41 -0800

    Added Tumblelog

--- payblog/blog/app/app.py
+++ payblog/blog/app/app.py
@@ -62,45 +62,45 @@ class Entry(flask_db.Model):
         fts_entry.content = '\n'.join((self.title, self.content))
         fts_entry.save(force_insert=force_insert)
 
-@classmethod
-def public(cls):
-    return Entry.select().where(Entry.published == True)
-
-@classmethod
-def search(cls, query):
-    words = [word.strip() for word in query.split() if word.strip()]
-    if not words:
-        # Return empty query.
-        return Entry.select().where(Entry.id == 0)
-    else:
-        search = ' '.join(words)
-
-    return (FTSEntry
-            .select(
-                FTSEntry,
-                Entry,
-                FTSEntry.rank().alias('score'))
-            .join(Entry, on=(FTSEntry.entry_id == Entry.id).alias('entry'))
-            .where(
-                (Entry.published == True) &
-                (FTSEntry.match(search)))
-            .order_by(SQL('score').desc()))
-
-@classmethod
-def drafts(cls):
-    return Entry.select().where(Entry.published == False)
-
-@property
-def html_content(self):
-    hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
-    extras = ExtraExtension()
-    markdown_content = markdown(self.content, extensions=[hilite, extras])
-    oembed_content = parse_html(
-        markdown_content,
-        oembed_providers,
-        urlize_all=True,
-        maxwidth=app.config['SITE_WIDTH'])
-    return Markup(oembed_content)
+    @classmethod
+    def public(cls):
+        return Entry.select().where(Entry.published == True)
+    
+    @classmethod
+    def search(cls, query):
+        words = [word.strip() for word in query.split() if word.strip()]
+        if not words:
+            # Return empty query.
+            return Entry.select().where(Entry.id == 0)
+        else:
+            search = ' '.join(words)
+    
+        return (FTSEntry
+                .select(
+                    FTSEntry,
+                    Entry,
+                    FTSEntry.rank().alias('score'))
+                .join(Entry, on=(FTSEntry.entry_id == Entry.id).alias('entry'))
+                .where(
+                    (Entry.published == True) &
+                    (FTSEntry.match(search)))
+                .order_by(SQL('score').desc()))
+    
+    @classmethod
+    def drafts(cls):
+        return Entry.select().where(Entry.published == False)
+    
+    @property
+    def html_content(self):
+        hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
+        extras = ExtraExtension()
+        markdown_content = markdown(self.content, extensions=[hilite, extras])
+        oembed_content = parse_html(
+            markdown_content,
+            oembed_providers,
+            urlize_all=True,
+            maxwidth=app.config['SITE_WIDTH'])
+        return Markup(oembed_content)
 
 class FTSEntry(FTSModel):
     entry_id = IntegerField()
