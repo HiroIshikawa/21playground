f3d4df32f14be10ce799c215fcc1340b6e6112d7
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-16 22:47:49 +0900

    Added config and forms

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,21 @@
+from flask import 
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Tak'} # fake user
+	posts = [
+	{
+		'author': {'nickname': 'Tak'},
+		'body': 'Beautiful day in Tokyo!'
+	},
+	{
+		'author': {'nickname': 'Susan'},
+		'body': 'This is it!'
+	}
+	]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
