f35ab12b1735c2a91b747fef7844f72a1aa81b55
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-18 02:31:30 +0900

    Added login auth

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID 
+from config import basedir
+
+lm = LoginManager()
+lom.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app = Flask(__name__)
 app.config.from_object('config')
