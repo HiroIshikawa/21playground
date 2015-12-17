5d689a921bb485bc9d6440363203f1cc9f37afee
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-17 13:31:54 +0900

    Added form view

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,5 +1,6 @@
-from flask import 
+from flask import render_template, flask, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +20,10 @@ def index():
 							title='Home',
 							user=user,
 							posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html',
+							title='Sign In',
+							form=form)
