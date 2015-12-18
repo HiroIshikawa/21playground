97f89ee7b4f2daecd7c1412f27c1256f51c7570b
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-18 15:28:15 +0900

    Added user avator and edit form

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,7 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(Form):
 	openid = StringField('openid', validators=[DataRequired()])
 	remember_me = BooleanField('remember_me', default=False)
+	about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
--- microblog/app/views.py
+++ microblog/app/views.py
@@ -3,6 +3,8 @@ from flask.ext.login import login_user, logout_user, current_user, login_require
 from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
+from datetime import datetime
+from forms import LoginForm, EditForm
 
 @app.route('/')
 @app.route('/index')
@@ -71,6 +73,11 @@ def after_login(resp):
 @app.before_request
 def before_request():
 	g.user = current_user
+	if g.user.is_authenticated:
+		g.user.last_seen = datetime.utcnow()
+		db.session.add(g.user)
+		db.session.commit()
+
 
 @app.route('/user/<nickname>')
 @login_required
@@ -86,3 +93,21 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+from forms import LoginForm, EditForm
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
