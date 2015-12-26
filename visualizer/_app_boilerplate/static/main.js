fc1bbb85332d1d2d6aa2d03a65733684773c696c
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-27 00:19:02 +0900

    Added bubble configuration

--- visualizer/_app_boilerplate/static/main.js
+++ visualizer/_app_boilerplate/static/main.js
@@ -11,4 +11,10 @@ function createGraph() {
 	var format = d3.format(",d");  // convert value to integer
 	var color = d3.scale.category20();  // create ordinal scale with 20 colors
 	var sizeOfRadius = d3.scale.pow().domain([-100,100]).range([-50,50]);  // https://github.com/mbostock/d3/wiki/Quantitative-Scales#pow
+	var bubble = d3.layout.pack()
+	.sort(null)  // disable sorting, use DOM tree traversal
+	.size([width, height])  // chart layout size
+	.padding(1)  // padding between circles
+	.radius(function(d) { return 20 + (sizeOfRadius(d) * 30); });  // radius for each circle
+
 }
