061d23f513d4ee492880b068b0483a7092e24f71
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-27 00:17:57 +0900

    Added d3 figure configuration

--- visualizer/_app_boilerplate/static/main.js
+++ visualizer/_app_boilerplate/static/main.js
@@ -6,4 +6,9 @@ $(function() {
 
 function createGraph() {
   // Code goes here
+	var width = 960; // chart width
+	var height = 700; // chart height
+	var format = d3.format(",d");  // convert value to integer
+	var color = d3.scale.category20();  // create ordinal scale with 20 colors
+	var sizeOfRadius = d3.scale.pow().domain([-100,100]).range([-50,50]);  // https://github.com/mbostock/d3/wiki/Quantitative-Scales#pow
 }
