370a5cc81f1d28e3d7df1f36f5c9184320c568f8
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-31 00:58:26 +0900

    Added the queued job with redis server

--- flask-by-example/app.py
+++ flask-by-example/app.py
@@ -21,58 +21,67 @@ q = Queue(connection=conn)
 
 from models import *
 
-#########
-# rutes #
-#########
+##########
+# helper #
+##########
+
+def count_and_save_words(url):
+
+	errors = []
+
+	try:
+		r = requests.get(url)
+	except:
+		errors.append(
+			"Unable to get URL. Please make sure it's valid nad try again"
+		)
+		return {"error": errors}
+
+	# text processing
+	raw = BeautifulSoup(r.text).get_text()
+	nltk.data.path.append('./nltk_data/') # set the path
+	tokens = nltk.word_tokenize(raw)
+	text = nltk.Text(tokens)
+
+	# remove punctuation, count raw words
+	nonPunct = re.compile('.*[A-Za-z].*')
+	raw_words = [w for w in text if nonPunct.match(w)]
+	raw_word_count = Counter(raw_words)
+
+	# stop words
+	no_stop_words = [w for w in raw_words if w.lower() not in stops]
+	no_stop_words_count = Counter(no_stop_words)
+
+	# save the resulsts
+	try:
+		result = Result(
+			url=url,
+			result_all=raw_word_count,
+			result_no_stop_words=no_stop_words_count
+		)
+		db.session.add(result)
+		db.session.commit()
+		return result.id
+	except:
+		errors.append("Unable to add item to database.")
+		return {"error": errors}
+
+##########
+#  rutes #
+##########
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	errors = []
 	results = {}
 	if request.method == "POST":
 		# get url that the user had entered
-		try:
-			url = request.form['url']
-			r = requests.get(url)
-		except:
-			errors.append(
-				"Unable to get URL. Please make sure it's valid and try agian."
-			)
-			return render_template('index.html', errors=errors)
-		if r:
-
-			# text processing
-			raw = BeautifulSoup(r.text).get_text()
-			nltk.data.path.append('./nltk_data/') # set the path
-			tokens = nltk.word_tokenize(raw)
-			text = nltk.Text(tokens)
-
-			# remove punctuation, count raw words
-			nonPunct = re.compile('.*[A-Za-z].*')
-			raw_words = [w for w in text if nonPunct.match(w)]
-			raw_word_count = Counter(raw_words)
-
-			# stop words
-			no_stop_words = [w for w in raw_words if w.lower() not in stops]
-			no_stop_words_count = Counter(no_stop_words)
-
-			# save the results
-			results = sorted(
-				no_stop_words_count.items(),
-				key=operator.itemgetter(1),
-				reverse=True
-			)[:10]
-			try:
-				result = Result(
-					url = url,
-					result_all=raw_word_count,
-					result_no_stop_words=no_stop_words_count
-				)
-				db.session.add(result)
-				db.session.commit()
-			except:
-				errors.append("Unable to add item to database.")
-	return render_template('index.html', errors=errors, results=results)
-
+		url = request.form['url']
+		if 'http://' not in url[:7]:
+			url = 'http://' + url
+		job = q.enqueue_call(
+			func=count_and_save_words, args=(url,), result_ttl=5000
+		)
+		print(job.get_id())
+		
 if __name__ == '__main__':
     app.run()
