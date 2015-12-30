4310c74ce42d3b189150911ebf8ad8e8c86f23ff
HiroIshikawa <hiro52ishikawa@gmail.com>
2015-12-30 23:00:21 +0900

    Added text processing unit for a url

--- flask-by-example/app.py
+++ flask-by-example/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stop_words
+from collections import Counter
+from bs4 import BeautifulSoup
+import operator
 import os
+import requests
+import re
+import nltk
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -8,14 +16,58 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+#########
+# rutes #
+#########
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	errors = []
+	results = {}
+	if request.method == "POST":
+		# get url that the user had entered
+		try:
+			url = request.form['url']
+			r = requests.get(url)
+		except:
+			errors.append(
+				"Unable to get URL. Please make sure it's valid and try agian."
+			)
+			return render_template('index.html', errors=errors)
+		if r:
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+			# text processing
+			raw = BeautifulSoup(r.text).get_text()
+			nltk.data.path.append('./nltk_data/') # set the path
+			tokens = nltk.word_tokenize(raw)
+			text = nltk.Text(tokens)
+
+			# remove punctuation, count raw words
+			nonPunct = re.compile('.*[A-Za-z].*')
+			raw_words = [w for w in text if nonPunct.match(w)]
+			raw_word_count = Counter(raw_words)
+
+			# stop words
+			no_stop_words = [w for w in raw_words if w.lower() not in stops]
+			no_stop_words_count = Counter(no_stop_words)
+
+			# save the results
+			results = sorted(
+				no_stop_words_count.items(),
+				key=operator.itemgetter(1),
+				reverse=True
+			)
+			try:
+				result = Result(
+					url = url,
+					result_all=raw_word_count,
+					result_no_stop_words=no_stop_words_count
+				)
+				db.session.add(result)
+				db.session.commit()
+			except:
+				errors.append("Unable to add item to database.")
+	return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
     app.run()
