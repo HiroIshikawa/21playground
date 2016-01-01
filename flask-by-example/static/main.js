e677afa8a8ed8d215a86a9fb47b8d2a995b0f2e1
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-01-01 10:13:16 +0900

    Added the angular configuration on the main.js and index.html

--- flask-by-example/static/main.js
+++ flask-by-example/static/main.js
@@ -3,8 +3,29 @@
 	'use strict';
 
 	angular.module('WordcountApp', [])
-
 	.controller('WordcountController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+	function getWordCount(jobID) {
+
+		var timeout = "";
+
+		var poller = function() {
+		// fire another request
+			$http.get('/results/'+jobID).
+				success(function(data, status, headers, config) {
+			    	if(status === 202) {
+						$log.log(data, status);
+					} else if (status === 200){
+						$log.log(data);
+						$timeout.cancel(timeout);
+						return false;
+			    	}
+				    // continue to call the poller() function every 2 seconds
+				    // until the timeout is cancelled
+			    		timeout = $timeout(poller, 2000);
+			  		});
+			};
+			poller();
+	}
 	$scope.getResults = function() {
 
 		$log.log("test");
@@ -15,12 +36,18 @@
 		$http.post('/start', {"url": userInput}).
 			success(function(results) {
 				$log.log(results);
+				getWordCount(results);
 			}).
 			error(function(error) {
 				$log.log(error);
 			});
 	};
 	}
+
+	
+
+	
+	
 
 	]);
 }());
