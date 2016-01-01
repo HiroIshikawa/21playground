a83ea7390d04a53cdc88e02b24f49c146d988310
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-01-01 10:39:49 +0900

    Added the angular timeout injection

--- flask-by-example/static/main.js
+++ flask-by-example/static/main.js
@@ -3,7 +3,7 @@
 	'use strict';
 
 	angular.module('WordcountApp', [])
-	.controller('WordcountController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+	.controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
 	function getWordCount(jobID) {
 
 		var timeout = "";
@@ -11,20 +11,21 @@
 		var poller = function() {
 		// fire another request
 			$http.get('/results/'+jobID).
-				success(function(data, status, headers, config) {
+				success(function(data, status, headers, config, timeout) {
 			    	if(status === 202) {
 						$log.log(data, status);
 					} else if (status === 200){
 						$log.log(data);
+						$scope.wordcounts = data;
 						$timeout.cancel(timeout);
 						return false;
 			    	}
 				    // continue to call the poller() function every 2 seconds
 				    // until the timeout is cancelled
-			    		timeout = $timeout(poller, 2000);
-			  		});
-			};
-			poller();
+			    	timeout = $timeout(poller, 2000);
+			  	});
+		};
+		poller();
 	}
 	$scope.getResults = function() {
 
